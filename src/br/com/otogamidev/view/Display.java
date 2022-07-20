ba8dea831073e3990b3625575ff2910549c5686f
HenriqueMAP <henrique.map@outlook.com>
2022-07-20 02:36:33 -0300

    Lecture 268 - Implementation of the display structure.

--- src/br/com/otogamidev/view/Display.java
+++ src/br/com/otogamidev/view/Display.java
@@ -1,10 +1,19 @@
 package br.com.otogamidev.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Display extends JPanel {
 
-    public Display() {
+    private final JLabel label;
 
+    public Display() {
+        setBackground(new Color(46,49,50));
+        label = new JLabel("1234,56");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+        add(label);
     }
 }
