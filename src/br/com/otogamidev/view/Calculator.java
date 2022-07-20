76ea0b5c91f4fc139402058bb8e235fd62aa9477
HenriqueMAP <henrique.map@outlook.com>
2022-07-19 21:39:50 -0300

    Lecture 266 - Starting calculator design and implementation of the first screen.

--- src/br/com/otogamidev/view/Calculator.java
+++ src/br/com/otogamidev/view/Calculator.java
@@ -1,4 +1,24 @@
 package br.com.otogamidev.view;
 
-public class Calculator {
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Calculator extends JFrame {
+
+
+    public Calculator(){
+        final int screenWidth = 232;
+        final int screenHeight = 322;
+        final Component windowCenteredOnTheScreen = null;
+
+        setSize(screenWidth, screenHeight);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(windowCenteredOnTheScreen);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Calculator();
+    }
 }
