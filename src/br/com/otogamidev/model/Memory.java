6c2626c2644d17448e6768367c21bb70b245d31c
HenriqueMAP <henrique.map@outlook.com>
2022-07-24 23:23:20 -0300

    Lecture 275 - Implementation of memory processing from user-supplied inputs.

--- src/br/com/otogamidev/model/Memory.java
+++ src/br/com/otogamidev/model/Memory.java
@@ -7,38 +7,80 @@ public class Memory {
 
     private static final Memory singletonInstance = new Memory();
     private final List<ObserverMemory> observers = new ArrayList<>();
+    private CommandType lastCommandType = null;
+    private boolean toReplace = false;
     private String actualText = "";
+    private String bufferedText = "";
 
-    private Memory(){
-
-    }
+    private Memory(){ }
 
     private enum CommandType { RESET, NUMBER, DIVISION, MULTIPLICATION, SUM, SUBTRACTION, EQUAL, COMMA; };
-
-    public List<ObserverMemory> getObservers() {
-        return observers;
-    }
-
-    public void setActualText(String actualText) {
-        this.actualText = actualText;
-    }
-
-    public String getActualText() {
-        return actualText.isEmpty() ? "0" : actualText;
-    }
 
     public static Memory getSingletonInstance(){
         return singletonInstance;
     }
 
+    public List<ObserverMemory> getObservers() { return observers; }
+
     public void addObserver(final ObserverMemory observer) {
         getObservers().add(observer);
     }
 
-    public void processCommand(final String newValue) {
-        CommandType commandType = detectCommandType(newValue);
-        System.out.println(commandType);
-        setActualText(getActualText().concat(newValue));
+    public CommandType getLastCommandType() { return lastCommandType; }
+
+    public void setLastCommandType(final CommandType lastCommandType) { this.lastCommandType = lastCommandType; }
+
+    public boolean isToReplace() { return toReplace; }
+
+    public void setToReplace(final boolean toReplace) { this.toReplace = toReplace; }
+
+    public void setActualText(final String actualText) { this.actualText = actualText; }
+
+    public String getActualText() {
+        return actualText.isEmpty() ? "0" : actualText;
+    }
+
+    public String getBufferedText() { return bufferedText; }
+
+    public void setBufferedText(final String bufferedText) { this.bufferedText = bufferedText; }
+
+    public void processCommand(final String typedValue) {
+        CommandType commandType = detectCommandType(typedValue);
+        if(commandType == null){
+            return;
+        } else {
+            switch(commandType){
+                case RESET:
+                    setActualText("");
+                    setBufferedText("");
+                    setToReplace(false);
+                    setLastCommandType(null);
+                    break;
+                case NUMBER:
+                    if(getActualText().equals("0")){
+                        setToReplace(true);
+                    }
+                case COMMA:
+                    setActualText(isToReplace() ? typedValue : getActualText().concat(typedValue));
+                    setToReplace(false);
+                    break;
+                case DIVISION:
+                    setActualText("");
+                    break;
+                case MULTIPLICATION:
+                    setActualText("");
+                    break;
+                case SUM:
+                    setActualText("");
+                    break;
+                case SUBTRACTION:
+                    setActualText("");
+                    break;
+                case EQUAL:
+                    setActualText("");
+                    break;
+            }
+        }
         getObservers().forEach(observer -> observer.changeValue(getActualText()));
     }
 
@@ -47,19 +89,19 @@ public class Memory {
             Integer.parseInt(valueToBeVerified);
             return CommandType.NUMBER;
         } catch (NumberFormatException numberFormatException){
-            if("AC".equals(valueToBeVerified)){
+            if(valueToBeVerified.equals("AC")){
                 return CommandType.RESET;
-            } else if ("/".equals(valueToBeVerified)){
+            } else if (valueToBeVerified.equals("/")){
                 return CommandType.DIVISION;
-            } else if ("X".equals(valueToBeVerified)){
+            } else if (valueToBeVerified.equals("X")){
                 return CommandType.MULTIPLICATION;
-            } else if ("+".equals(valueToBeVerified)){
+            } else if (valueToBeVerified.equals("+")){
                 return CommandType.SUM;
-            } else if ("-".equals(valueToBeVerified)){
+            } else if (valueToBeVerified.equals("-")){
                 return CommandType.SUBTRACTION;
-            } else if ("=".equals(valueToBeVerified)){
+            } else if (valueToBeVerified.equals("=")){
                 return CommandType.EQUAL;
-            } else if (",".equals(valueToBeVerified)){
+            } else if ((valueToBeVerified.equals(",")) && (getActualText().contains(",") == false)){
                 return CommandType.COMMA;
             }
         }
